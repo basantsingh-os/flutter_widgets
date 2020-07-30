6379cb6a200aaed77de5e455be946fb98f60d6a3
Basant Singh <you@example.com>
2020-07-31 02:08:20 +0530

    Bottom Navigation Bar

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       new MaterialApp(
         theme: new ThemeData(
-          primarySwatch: Colors.teal,
+          primarySwatch: Colors.black26,
         ),
         home: new HomePage(),
       ),
@@ -14,54 +14,113 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+  TabController tabController;
+  @override
+  void initState() {
+    super.initState();
+    tabController = new TabController(length: 2, vsync: this);
+  }
+  @override 
+  void dispose(){
+    tabController.dispose();
+    super.dispose();
+  }
+
+  // int current_step = 0;
+  // List<Step> my_steps = [
+  //   new Step(
+  //       title: new Text("Step 1"),
+  //       content: new Text("Some Content 1"),
+  //       isActive: true),
+  //   new Step(
+  //       title: new Text("Step 2"),
+  //       content: new Text("Some Content 3"),
+  //       isActive: true),
+  //   new Step(
+  //       title: new Text("Step 3"),
+  //       content: new Text("Some Content 3"),
+  //       isActive: true)
+  // ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(
-        title: new Text("Utils App"),
-        backgroundColor: Colors.black26,
-      ),
-      body: new MyBody(),
-    );
+        appBar: new AppBar(
+          title: new Text("Utils App"),
+          backgroundColor: Colors.black26,
+        ),
+        body: new TabBarView(
+          children: <Widget>[
+            new NewPage("first"),
+            new NewPage("Second"),
+          ],
+          controller: tabController,
+        ),
+        bottomNavigationBar: new Material(
+          color:Colors.black26,
+          child:new TabBar(
+            controller:tabController,
+            tabs: <Widget>[
+            new Tab(
+              icon:new Icon(Icons.favorite),
+
+            ),
+            new Tab(
+              icon:new Icon(Icons.email),
+
+            )
+
+          ],
+          ),
+
+        ),
+        );
   }
 }
-
-class MyBody extends StatelessWidget {
-  AlertDialog dialog = new AlertDialog(
-    content:new Text(
-      "Dialog is up"
-      ,style:new TextStyle(
-        fontSize: 30
-        ),
-        
-        ),
-        title:new Text(
-          "Lol",
-          style:new TextStyle(
-            fontSize:40
-          )
-        )
-  );
+class NewPage extends StatelessWidget {
+  final String title;
+  NewPage(this.title);
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Center(
-      child: new RaisedButton(
-          child:
-              new Text("click Me", style: new TextStyle(color: Colors.white)),
-          color: Colors.black26,
-
-          ///code for snackbar ////////////////
-          // onPressed:()=>Scaffold.of(context).showSnackBar(
-          //   new SnackBar(
-          //     content:new Text("I am pressed"),
-          //     duration:new Duration(seconds:2)
-          //   )
-          // )
-          ///////////////////////////////////// .
-
-          onPressed: () => showDialog(context: context, child: dialog)),
-    ));
+    return Scaffold(body: new Center(child: new Text(title)));
   }
 }
+
+// class MyBody extends StatelessWidget {
+//   AlertDialog dialog = new AlertDialog(
+//     content:new Text(
+//       "Dialog is up"
+//       ,style:new TextStyle(
+//         fontSize: 30
+//         ),
+
+//         ),
+//         title:new Text(
+//           "Lol",
+//           style:new TextStyle(
+//             fontSize:40
+//           )
+//         )
+//   );
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//         child: Center(
+//       child: new RaisedButton(
+//           child:
+//               new Text("click Me", style: new TextStyle(color: Colors.white)),
+//           color: Colors.black26,
+
+//           ///code for snackbar ////////////////
+//           // onPressed:()=>Scaffold.of(context).showSnackBar(
+//           //   new SnackBar(
+//           //     content:new Text("I am pressed"),
+//           //     duration:new Duration(seconds:2)
+//           //   )
+//           // )
+//           ///////////////////////////////////// .
+
+//           onPressed: () => showDialog(context: context, child: dialog)),
+//     ));
+//   }
+// }
